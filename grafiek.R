efec7bbe43393308c5f3d01a757dc639c3d15a11
yaidendejong <yaidendejong@hotmail.com>
2025-06-05 12:41:58 +0200

    Update grafiek.R

--- grafiek.R
+++ grafiek.R
@@ -5,7 +5,6 @@ library(ggplot2)
 colnames(Studieschuld_Alle_Landen)
 
 colnames(Studieschuld_Alle_Landen) <- c("year", "nederland", "engeland", "usa")
-http://127.0.0.1:39713/graphics/17f98e2e-2150-4a3e-abdb-cac3eb88fbc9.png
 library(tidyr)
 
 long_data <- pivot_longer(Studieschuld_Alle_Landen, 
@@ -23,3 +22,4 @@ ggplot(long_data, aes(x = year, y = schuld, color = land)) +
        y = "Studieschuld (€)",
        color = "Land") +
   theme_minimal()
+
